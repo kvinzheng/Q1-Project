3b521d715ee0a8233abe8a53dae0eb8644bd7132
Kevin(Jiahong) Zheng <kvinzheng@gmail.com>
2017-02-07 19:57:44 -0800

    I just make the selection work now , now the users can modified what kind of selection they want, they dont' have to choose all three

--- application.js
+++ application.js
@@ -1,16 +1,7 @@
-// let APIKey = localStorage.APIKey;
-// console.log(APIKey);
 $(document).ready(function() {
   $('select').material_select();
 });
 
-function loadScript() {
-      var script = document.createElement('script');
-      script.type = 'text/javascript';
-      script.src = 'https://maps.googleapis.com/maps/api/js?' +
-      'key=' + GOOGLE_MAP_KEY +'&callback=initMap'; //& needed
-      document.body.appendChild(script);
-    }
 
 function initMap(){
   //map options
@@ -43,112 +34,116 @@ function getData(){
   let url = ('https://data.sfgov.org/resource/vv57-2fgy.json');
   let incidents = [];
   return fetch(url).then(function(respo){
-    //console.log(respo.json());
     return respo.json();
   })
   .then(function(resJson){
     resJson.forEach(function(incident){
       incidents.push(incident);
     })
-    //console.log(incidents.length);
-    //console.log(resJson);
     return incidents;
   })
   .catch(function(err){
     console.log(err);
   });
-
 }
 
 let form = document.getElementsByTagName('form')[0];
 form.addEventListener("submit", function(e){
   e.preventDefault();
+  //i empty the table here
+
   let timeSelector = document.getElementById("selection-time");
   let dayofweekSelector = document.getElementById("selection-day");
   let sfdistrictSelector = document.getElementById("selection-district");
-  // console.log(timeSelector.value, dayofweekSelector.value, sfdistrictSelector.value, "KJLKJLKJLKJL");
 
-  getData().then(function(incidents) {
-    console.log(incidents);
-    // let timesArray = [];
-    // let dayArray = [];
-    // let districtArray =[];
+  getData()
+    .then(function(incidents) {
+      $('#myTableID').empty();
 
-    // REFACTOR TO HAVE 1 MAP THROUGH incidents
-    // incidents.forEach(function(incident){
-    //  timesArray.push(incident.time.split(':')[0]);
-    //  dayArray.push(incident.dayofweek);
-    //  districtArray.push(incident.pddistrict);
-    // });
-    // console.log(timesArray);
-    // console.log(dayArray);
-    // console.log(districtArray);
+      //console.log(document.getElementById('myTableID'));
+      console.log(incidents);
+      let sum = 0;
 
-    let sum = 0;
-    //i will fix this duplication
-    //ask matt about this one more time
+      incidents.forEach(function(incident){
+        if( (timeSelector.value === incident.time.split(':')[0]) && (dayofweekSelector.value === incident.dayofweek) && (sfdistrictSelector.value === incident.pddistrict) ){
+          sum = sum + 1;
+        }
+      });
 
-    incidents.forEach(function(incident){
-      if( (timeSelector.value === incident.time.split(':')[0]) && (dayofweekSelector.value === incident.dayofweek) && (sfdistrictSelector.value === incident.pddistrict) ){
-        sum = sum + 1;
+      let numberofincident = document.getElementById("numberofIncident");
+      numberofincident.innerText = sum;
+      console.log(sum);
+      //
+      // let allThreeFilter = incidents.filter(function(incident){
+      //   //if time has no value
+      //   return ( incident.time.split(':')[0] === timeSelector.value ) &&
+      //          ( incident.pddistrict === sfdistrictSelector.value)
+      //
+      //   return ( incident.dayofweek === dayofweekSelector.value ) &&
+      //          ( incident.pddistrict === sfdistrictSelector.value)
+      //   // else
+      //   return ( incident.time.split(':')[0] === timeSelector.value ) &&
+      //          ( incident.dayofweek === dayofweekSelector.value ) &&
+      //          ( incident.pddistrict === sfdistrictSelector.value)
+      // })
+
+      let allThreeFilter = incidents.slice();
+
+      //if time value exist
+      if(timeSelector.value){
+        allThreeFilter = allThreeFilter.filter(function(incident){
+          return ( incident.time.split(':')[0] === timeSelector.value )
+        });
       }
-    });
+      //if day value exist
+      if(dayofweekSelector.value){
+        allThreeFilter = allThreeFilter.filter(function(incident){
+          return ( incident.dayofweek === dayofweekSelector.value )
+        });
+      }
+      //if district exist
+      if(sfdistrictSelector.value){
+        allThreeFilter = allThreeFilter.filter(function(incident){
+          return ( incident.pddistrict === sfdistrictSelector.value )
+        });
+      }
 
-    let numberofincident = document.getElementById("numberofIncident");
-    numberofincident.innerText = sum;
-    console.log(sum);
+      let topFiveAllThree = allThreeFilter.sort(function(a,b){
+        return b.date - a.date;
+        return Date.parse(b.date.replace(/'-'/g, '/') - Data.parse(a.date.replace(/'-'/g, '/')))
+      }).slice(0,6)
 
-    //I am sorting the top five object with selection of time with in the order of timestamp.
+      console.log(topFiveAllThree);
 
-    let allThreeFilter = incidents.filter(function(incident){
-      // console.log(incident.dayofweek);
-      // console.log(dayofweekSelector.value);
-      return ( incident.time.split(':')[0] === timeSelector.value ) &&
-             ( incident.dayofweek === dayofweekSelector.value ) &&
-             ( incident.pddistrict === sfdistrictSelector.value)
-    })
-
-    console.log(allThreeFilter);
-    let topFiveAllThree = allThreeFilter.sort(function(a,b){
-      return a.data - b.data;
-    }).slice(0,6)
-    console.log(topFiveAllThree);
-
-
-    appendTable()
+      appendTable(topFiveAllThree);
     });
   });
 
   //here is my append table
-
   let selectClass = document.getElementsByClassName("select");
   let tbody = document.querySelector("tbody");
 
-  function appendTable(){
-    let row = document.createElement("tr");
-    let time  = document.createElement("td");
-    let day = document.createElement("td");
-    let date = document.createElement("td");
-    let district = document.createElement("td");
-    let address = document.createElement("td");
+  function appendTable(topFiveAllThree){
+    for(let i = 0; i < topFiveAllThree.length; i++){
+      let row = document.createElement("tr");
+      let time  = document.createElement("td");
+      let day = document.createElement("td");
+      let date = document.createElement("td");
+      let district = document.createElement("td");
+      let address = document.createElement("td");
 
-    time.innerText = incident.time.split(':')[0];
-    day.innerText = incident.dayofweek;
-    date.innerText = incident.date;
-    district.innerText = incident.pddistrict;
-    address.innerText = incident.address;
+      time.innerText = topFiveAllThree[i].time.split(':')[0]; //my click value
+      day.innerText = topFiveAllThree[i].dayofweek; //my cl
+      date.innerText = topFiveAllThree[i].date;
+      district.innerText = topFiveAllThree[i].pddistrict;
+      address.innerText = topFiveAllThree[i].address;
 
-    // var selectTime = document.getElementById("selection-time");
-    // var selectDay = document.getElementById("selection-day");
-    // var selectDistrict = document.getElementById("selection-district");
-
-    // var selectDay = this.getAttribute('name');
-    // var selectDistrict = this.getAttribute('name');
-    row.appendChild(time);
-    row.appendChild(day);
-    row.appendChild(date);
-    row.appendChild(district);
-    row.appendChild(address);
-    tbody.appendChild(row);
+      row.appendChild(time);
+      row.appendChild(day);
+      row.appendChild(date);
+      row.appendChild(district);
+      row.appendChild(address);
+      tbody.appendChild(row);
+    }
     return tbody;
   }
